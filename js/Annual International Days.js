aa7e5404afedad90dfdc223a0a261e55ceb3852b
mrlaboratory <admin@mrlaboratory.com>
2021-01-22 23:36:32 +0600

    add day notifyer

---
+++ js/Annual International Days.js
@@ -0,0 +1,406 @@
+var di = new Date();
+var dday = di.getDate();
+var dmonth = di.getMonth();
+var iDate= di.getFullYear()+'-'+(di.getMonth()+1)+'-'+di.getDate();
+var monthDate=dmonth+"."+dday;
+var monthDate = monthDate.toString()
+var gSeacrhLink = "https://www.google.com/search?q=mr laboratory";
+// $(".center .date").text(monthDate);
+var iDay;
+switch(monthDate){
+// 0.4. January = World Braille Day
+case "0.4": iDay = "World Braille Day";  break;
+
+// 0.24 January = International Day of Education
+case "0.24": iDay = "International Day of Education"; break;
+
+// 0.27. January International Day of Commemoration in Memory of the Victims of the Holocaust
+case "0.27": iDay = "International Day of Commemoration in Memory of the Victims of the Holocaust";  break;
+
+// 1.4. February  = World Cancer Day
+case "1.4": iDay = "World Cancer Day";  break;
+
+// 1.6. February = International Day of Zero Tolerance to Female Genital Mutilation
+case "1.6": iDay = "International Day of Zero Tolerance to Female Genital Mutilation";  break;
+
+// 1.10. February = World Pulses Day
+case "1.10": iDay = "World Pulses Day";  break;
+
+// 1.11. February = International Day of Women and Girls in Science
+case "1.11": iDay = "International Day of Women and Girls in Science";  break;
+
+// 1.13. February = World Radio Day
+case "1.13": iDay = "World Radio Day";  break;
+
+// 1.20. February = World Day of Social Justice
+case "1.20": iDay = "World Day of Social Justice";  break;
+
+// 1.21. February = International Mother Language Day
+case "1.21": iDay = "February = International Mother Language Day";  break;
+
+// 2.1. March = Zero Discrimination Day
+case "2.1": iDay = "Zero Discrimination Day";  break;
+
+// 2.3. March = World Wildlife Day
+case "2.3": iDay = "World Wildlife Day";  break;
+
+// 2.8. March  = International Women's Day
+case "2.8": iDay = "International Women's Day";  break;
+
+// 2.20. March = International Day of Happiness
+case "2.20": iDay = "International Day of Happiness";  break;
+
+// 2.21. March = International Day for the Elimination of Racial Discrimination & World Poetry Day & International Day of Nowruz & World Down Syndrome Day & International Day of Forests
+case "2.21": iDay = "International Day for the Elimination of Racial Discrimination & World Poetry Day & International Day of Nowruz & World Down Syndrome Day & International Day of Forests";  break;
+
+// 2.22. March = World Water Day
+case "2.22": iDay = "World Water Day";  break;
+ 
+// 2.23. March = World Meteorological Day
+case "2.23": iDay = "World Meteorological Day";  break;
+
+// 2.24. March = World Tuberculosis Day
+case "2.24": iDay = "World Tuberculosis Day";  break;
+
+// 2.24. March = International Day for the Right to the Truth concerning Gross Human Rights Violations and for the Dignity of Victims
+case "2.24": iDay = "International Day for the Right to the Truth concerning Gross Human Rights Violations and for the Dignity of Victims";  break;
+
+// 2.25. March = International Day of Remembrance of the Victims of Slavery and the Transatlantic Slave Trade & International Day of Solidarity with Detained and Missing Staff Members
+case "2.25": iDay = "International Day of Remembrance of the Victims of Slavery and the Transatlantic Slave Trade & International Day of Solidarity with Detained and Missing Staff Members";  break;
+
+// 3.2. April = World Autism Awareness Day
+case "3.2": iDay = "World Autism Awareness Day";  break;
+
+// 2.24 April = International Day for Mine Awareness and Assistance in Mine Action
+case "2.24": iDay = "International Day for Mine Awareness and Assistance in Mine Action";  break;
+ 
+// 3.5. April = International Day of Conscience
+case "3.5": iDay = "International Day of Conscience";  break;
+
+// 3.6. April = International Day of Sport for Development and Peace
+case "3.6": iDay = "International Day of Sport for Development and Peace";  break;
+
+// 3.7. April = International Day of Reflection on the 1994 Genocide in Rwanda & World Health Day
+case "3.7": iDay = "International Day of Reflection on the 1994 Genocide in Rwanda & World Health Day";  break;
+
+// 3.12. April = International Day of Human Space Flight
+case "3.12": iDay = "International Day of Human Space Flight";  break;
+
+// 3.18. April = International Day For Monuments and Sites
+case "3.18": iDay = "International Day For Monuments and Sites";  break;
+
+// 3.21. April = World Creativity and Innovation Day
+case "3.21": iDay = "World Creativity and Innovation Day";  break;
+
+// 3.22. April = Earth Day
+case "3.22": iDay = "Earth Day";  break;
+
+// 3.23. April = World Book and Copyright Day
+case "3.23": iDay = "World Book and Copyright Day";  break;
+
+// 3.24. April = International Day of Multilateralism and Diplomacy for Peace
+case "3.24": iDay = "International Day of Multilateralism and Diplomacy for Peace";  break;
+
+// 3.25. April = International Delegate’s Day & World Malaria Day
+case "3.25": iDay = "International Delegate’s Day & World Malaria Day";  break;
+
+// 3.26. April = International Chernobyl Disaster Remembrance Day & World Intellectual Property Day
+case "3.26": iDay = "International Chernobyl Disaster Remembrance Day & World Intellectual Property Day";  break;
+
+// 3.28. April = World Day for Safety and Health at Work
+case "3.28": iDay = "World Day for Safety and Health at Work";  break;
+ 
+// 3.29. April = Day of Remembrance for all Victims of Chemical Warfare & International Dance Day
+case "3.29": iDay = "Day of Remembrance for all Victims of Chemical Warfare & International Dance Day";  break;
+
+// 3.30. April = International Jazz Day
+case "3.30": iDay = "International Jazz Day";  break;
+
+// 4.2. May = World Tuna Day
+case "4.2": iDay = "International Jazz Day";  break;
+
+// 4.3. May = World Press Freedom Day
+case "4.3": iDay = "World Press Freedom Day";  break;
+
+// 4.8 May Time of Remembrance and Reconciliation for Those Who Lost Their Lives During the Second World War
+case "4.8": iDay = "May Time of Remembrance and Reconciliation for Those Who Lost Their Lives During the Second World War";  break;
+
+// 4.9 May Time of Remembrance and Reconciliation for Those Who Lost Their Lives During the Second World War
+case "4.9": iDay = "May Time of Remembrance and Reconciliation for Those Who Lost Their Lives During the Second World War";  break;
+
+// 4.15. May = International Day of Families
+case "4.15": iDay = "International Day of Families";  break;
+
+// 4.16. May = International Day of Living Together in Peace & International Day of Light
+case "4.16": iDay = "International Day of Living Together in Peace & International Day of Light";  break;
+
+// 4.17. May = World Telecommunication and Information Society Day
+case "4.17": iDay = "World Telecommunication and Information Society Day";  break;
+
+// 4.20. May = World Bee Day
+case "4.20": iDay = "World Bee Day";  break;
+
+// 4.21. May = World Tea Day
+case "4.21": iDay = "World Tea Day";  break;
+
+// 4.21. May = World Day for Cultural Diversity for Dialogue and Development
+case "4.21": iDay = "World Day for Cultural Diversity for Dialogue and Development";  break;
+
+// 4.22. May = International Day for Biological Diversity
+case "4.22": iDay = "International Day for Biological Diversity";  break;
+
+// 4.23. May = International Day to End Obstetric Fistula
+case "4.23": iDay = "International Day to End Obstetric Fistula";  break;
+
+// 4.29. May = International Day of UN Peacekeepers
+case "4.29": iDay = "International Day of UN Peacekeepers";  break;
+
+// 4.31. May = World No-Tobacco Day
+case "4.31": iDay = "World No-Tobacco Day";  break;
+
+// 5.1. June = Global Day of Parents
+case "5.1": iDay = "World No-Tobacco Day";  break;
+
+// 5.3. June = World Bicycle Day
+case "5.3": iDay = "World Bicycle Day";  break;
+ 
+// 5.4. June = International Day of Innocent Children Victims of Aggression
+case "5.4": iDay = "International Day of Innocent Children Victims of Aggression";  break;
+
+// 5.5. June = World Environment Day & International Day for the Fight against Illegal, Unreported and Unregulated Fishing
+case "5.5": iDay = "World Environment Day & International Day for the Fight against Illegal, Unreported and Unregulated Fishing";  break;
+
+// 5.7. June = World Food Safety Day
+case "5.7": iDay = "World Food Safety Day";  break;
+
+// 5.8. June = World Oceans Day
+case "5.8": iDay = "World Oceans Day";  break;
+
+// 5.12. June = World Day Against Child Labour
+case "5.12": iDay = "World Day Against Child Labour";  break;
+
+// 5.13. June = International Albinism Awareness Day
+case "5.13": iDay = "International Albinism Awareness Day";  break;
+
+// 5.14. June = World Blood Donor Day
+case "5.14": iDay = "World Blood Donor Day";  break;
+
+// 5.15. June = World Elder Abuse Awareness Day
+case "5.15": iDay = "World Elder Abuse Awareness Day";  break;
+
+// 5.16. June = International Day of Family Remittances
+case "5.16": iDay = "International Day of Family Remittances";  break;
+
+// 5.17. June = World Day to Combat Desertification and Drought
+case "5.17": iDay = "World Day to Combat Desertification and Drought";  break;
+
+// 5.18. June = Sustainable Gastronomy Day
+case "5.18": iDay = "Sustainable Gastronomy Day";  break;
+
+// 5.19. June = International Day for the Elimination of Sexual Violence in Conflict
+case "5.19": iDay = "International Day for the Elimination of Sexual Violence in Conflict";  break;
+
+// 5.20. June = World Refugee Day
+case "5.20": iDay = "World Refugee Day";  break;
+
+// 5.21. June = International Day of Yoga & International Day of the Celebration of the Solstice
+case "5.21": iDay = "International Day of Yoga & International Day of the Celebration of the Solstice";  break;
+
+// 5.23. June = United Nations Public Service Day & International Widows' Day
+case "5.23": iDay = "United Nations Public Service Day & International Widows' Day";  break;
+
+// 5.25. June = Day of the Seafarer
+case "5.25": iDay = "Day of the Seafarer";  break;
+
+// 5.26. June = International Day against Drug Abuse and Illicit Trafficking
+case "5.26": iDay = "International Day against Drug Abuse and Illicit Trafficking";  break;
+ 
+// 5.26. June  = United Nations International Day in Support of Victims of Torture
+case "5.26": iDay = "International Day against Drug Abuse and Illicit Trafficking";  break;
+
+// 5.29. June = International Day of the Tropics
+case "5.29": iDay = "International Day of the Tropics";  break;
+
+// 5.30. June = International Asteroid Day & International Day of Parliamentarism
+case "5.30": iDay = " International Asteroid Day";  break;
+
+// 6.11. July = World Population Day
+case "5.29": iDay = "World Population Day";  break;
+
+// 6.15. July = World Youth Skills Day
+case "5.15": iDay = "World Youth Skills Day";  break;
+
+// 6.18. July = Nelson Mandela International Day
+case "5.18": iDay = "Nelson Mandela International Day";  break;
+
+// 6.28. July = World Hepatitis Day
+case "5.28": iDay = "World Hepatitis Day";  break;
+
+// 6.30. July = International Day of Friendship
+case "5.30": iDay = "International Day of Friendship";  break;
+
+// 7.9. August = International Day of the World's Indigenous Peoples
+case "7.9": iDay = "International Day of Friendship";  break;
+
+// 7.12. August = International Youth Day
+case "7.12": iDay = "International Youth Day";  break;
+
+// 7.19. August = World Humanitarian Day
+case "7.19": iDay = "World Humanitarian Day";  break;
+
+// 7.21. August = International Day of Remembrance and Tribute to the Victims of Terrorism
+case "7.21": iDay = "International Day of Remembrance and Tribute to the Victims of Terrorism";  break;
+
+// 7.23. August =  International Day for the Remembrance of the Slave Trade and Its Abolition
+case "7.23": iDay = "International Day for the Remembrance of the Slave Trade and Its Abolition";  break;
+
+// 7.29. August = International Day against Nuclear Tests
+case "7.29": iDay = "International Day against Nuclear Tests";  break;
+
+// 7.30. August = International Day of the Victims of Enforced Disappearances
+case "7.30": iDay = "International Day of the Victims of Enforced Disappearances";  break;
+
+// 8.5. September = International Day of Charity
+case "8.5": iDay = "International Day of Charity";  break;
+
+// 8.8. September = International Literacy Day
+case "8.8": iDay = "International Literacy Day";  break;
+ 
+// 8.10. September = World Suicide Prevention Day
+case "8.10": iDay = "World Suicide Prevention Day";  break;
+
+// 8.12. September = United Nations Day for South-South Cooperation
+case "8.12": iDay = "United Nations Day for South-South Cooperation";  break;
+
+// 8.15. September =  International Day of Democracy
+case "8.15": iDay = "International Day of Democracy";  break;
+
+// 8.16. September = International Day for the Preservation of the Ozone Layer
+case "8.16": iDay = "International Day for the Preservation of the Ozone Layer";  break;
+
+// 8.1. September = International Day of Peacec
+// case 8.15 iDay = "International Day of Peacec";  break;
+
+// 8.23. September = International Day of Sign Languages
+case "8.23": iDay = "International Day of Sign Languages";  break;
+
+// 8.26. September = International Day for the Total Elimination of Nuclear Weapons
+case "8.26": iDay = "International Day for the Total Elimination of Nuclear Weapons";  break;
+
+// 8.27. September = World Tourism Day & Rabies Day
+case "8.27": iDay = "World Tourism Day & Rabies Day";  break;
+
+// 8.28. September = International Day for Universal Access to Information
+case "8.28": iDay = "International Day for Universal Access to Information";  break;
+
+// 8.30. September = International Translation Day
+case "8.30": iDay = "International Translation Day";  break;
+
+// 9.1. October = International Day of Older Persons
+case "9.1": iDay = "International Day of Older Persons";  break;
+
+// 9.2. October = International Day of Non-Violence
+case "9.2": iDay = "International Day of Non-Violence";  break;
+
+// 9.5. October =  World Teachers’ Day
+case "9.5": iDay = "World Teachers’ Day";  break;
+
+// 9.9. October = World Post Day
+case "9.9": iDay = "World Post Day";  break;
+
+// 9.10. October = World Mental Health Day
+case "9.10": iDay = "World Mental Health Day";  break;
+
+// 9.11. October = International Day of the Girl Child
+case "9.11": iDay = "International Day of the Girl Child";  break;
+
+// 9.13. October = International Day for Disaster Reduction
+case "9.13": iDay = "International Day for Disaster Reduction";  break;
+
+// 9.15. October = International Day of Rural Women & Global Handwashing Day
+case "9.15": iDay = "International Day of Rural Women & Global Handwashing Day";  break;
+
+// 9.16. October = World Food Day
+case "9.16": iDay = "World Food Day";  break;
+
+// 9.17. October = International Day for the Eradication of Poverty
+case "9.17": iDay = "International Day for the Eradication of Poverty";  break;
+
+// 9.24. October = United Nations Day & World Development Information Day
+case "9.24": iDay = "United Nations Day & World Development Information Day";  break;
+
+// 9.27. October = World Day for Audiovisual Heritage
+case "9.27": iDay = "World Day for Audiovisual Heritage";  break;
+
+// 9.31. October =  World Cities Day
+case "9.31": iDay = "World Cities Day";  break;
+
+// 10.2. November = International Day to End Impunity for Crimes against Journalists
+case "10.2": iDay = "International Day to End Impunity for Crimes against Journalists";  break;
+
+// 10.5. November = World Tsunami Awareness Day
+case "10.5": iDay = "World Tsunami Awareness Day";  break;
+
+// 10.6. November = International Day for Preventing the Exploitation of the Environment in War and Armed Conflict
+case "10.6": iDay = "International Day for Preventing the Exploitation of the Environment in War and Armed Conflict";  break;
+
+// 10.10. November = World Science Day for Peace and Development
+case "10.10": iDay = "World Science Day for Peace and Development";  break;
+
+// 10.14. November = World Diabetes Day
+case "10.14": iDay = "World Diabetes Day";  break;
+
+// 10.16. November = International Day for Tolerance
+case "10.16": iDay = "International Day for Tolerance";  break;
+
+// 10.19. November = World Toilet Day
+case "10.19": iDay = "World Toilet Day";  break;
+ 
+// 10.20. November = Universal Children’s Day
+case "10.20": iDay = "Universal Children’s Day";  break;
+
+// 10.21. November = World Television Day
+case "10.21": iDay = "World Television Day";  break;
+
+// 10.25. November = International Day for the Elimination of Violence against Women
+case "10.25": iDay = "International Day for the Elimination of Violence against Women";  break;
+
+// 10.29. November = International Day of Solidarity with the Palestinian People
+case "10.29": iDay = "International Day of Solidarity with the Palestinian People";  break;
+
+// 11.1. December = World AIDS Day
+case "11.1": iDay = "World AIDS Day";  break;
+ 
+// 11.2. December = International Day for the Abolition of Slavery
+case "11.2": iDay = "International Day for the Abolition of Slavery";  break;
+
+// 11.3. December = International Day of Persons with Disabilities
+case "11.3": iDay = "International Day of Persons with Disabilities";  break;
+
+// 11.5. December = International Volunteer Day for Economic and Social Development & World Soil Day
+case "11.5": iDay = "International Volunteer Day for Economic and Social Development & World Soil Day";  break;
+
+// 11.7. December = International Civil Aviation Day
+case "11.7": iDay = "International Civil Aviation Day";  break;
+
+// 11.9. December = International Anti-Corruption Day
+case "11.9": iDay = "International Anti-Corruption Day";  break;
+
+// 11.10. December = Human Rights Day
+case "11.10": iDay = "Human Rights Day";  break;
+
+// 11.11. December = International Mountain Day
+case "11.11": iDay = "International Mountain Day";  break;
+
+// 11.18. December = International Migrants Day
+case "11.18": iDay = "International Migrants Day";  break;
+
+// 11.20. December = International Human Solidarity Day
+case "11.20": iDay = "International Migrants Day";  break;
+
+default:iDay = "Have a nice day !" ; break;
+}
+var seemorelink ="<a target='_blank' href='https://www.google.com/search?q="+iDay+"'>See More ...</a>";
+
+// $(".center .nowday").text(iDay)
+dragNotify(`${da}-${mo}-${ye}`+" : </br> "+iDay+"</br>"+seemorelink);
