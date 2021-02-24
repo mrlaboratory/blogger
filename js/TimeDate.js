4da9e8297aa7170097eedd222ce19254d08734f6
MR Laboratory <58909573+mrlaboratory@users.noreply.github.com>
2021-02-24 16:36:03 +0600

    Update TimeDate.js

--- js/TimeDate.js
+++ js/TimeDate.js
@@ -44,9 +44,3 @@ const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
 const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
 const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
 ddate.text(`${da}-${mo}-${ye}`);
-if(dissableCopyText==true){
-  if (typeof document.onselectstart!="undefined" ) { document.onselectstart=new Function ("return false" );
-} else { document.onmousedown=new Function ("return false" ); document.onmouseup=new Function ("return true" );
-}
-
-}
