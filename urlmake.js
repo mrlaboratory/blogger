ab3584e721a21c32b1d2ea035488537e9462b8c1
mrlaboratory <mrlaboratory23@gmail.com>
2021-05-28 12:20:00 +0600

    url

--- urlmake.js
+++ urlmake.js
@@ -1,11 +1,10 @@
 var setting = {
 pengecualianstatus : true,
-pengecualian : ";ok.ru,bdstall.com,fb.com,copyLink(),awesome.top,worldometers.info,diigo.com,alltop.com,slashdot.org,triberr.com,growthhackers.com,whatsapp.com,instagram.com,adfty.biz,dribbble.com,patreon.com,folkd.com,pearltrees,dzone.com,instapaper.com,tracky.com,feedburner.com,gweb.com,public.sitejot.com,mrlaboratory.info,youtube.com,tumblr.com,play.app.goo.gl,stumbleupon.com,blogspot.com,messenger.com,mix.com,digg.com,bufferapp.com,reddit.com,pinterest.com,linkedin.com,youtu.be,m.me,blogger.com,1.bp.blogspot.com,mrlaboratory.com,facebook.com,twitter.com,javascript:;,mailto:";,
+pengecualian : &quot;ok.ru,bdstall.com,fb.com,copyLink(),awesome.top,worldometers.info,diigo.com,alltop.com,slashdot.org,triberr.com,growthhackers.com,whatsapp.com,instagram.com,adfty.biz,dribbble.com,patreon.com,folkd.com,pearltrees,dzone.com,instapaper.com,tracky.com,feedburner.com,gweb.com,public.sitejot.com,mrlaboratory.info,youtube.com,tumblr.com,play.app.goo.gl,stumbleupon.com,blogspot.com,messenger.com,mix.com,digg.com,bufferapp.com,reddit.com,pinterest.com,linkedin.com,youtu.be,m.me,blogger.com,1.bp.blogspot.com,mrlaboratory.com,facebook.com,twitter.com,javascript:;,mailto:&quot;,
 hanyauntukstatus : false,
-hanyauntuk : ";mrlaboratory.com";, 
-//use ::withpath for autolink specific path exampe.com/download/::withpath
-path : ";#?o=";
-};    
+hanyauntuk : &quot;mrlaboratory.com,mrlaboratory.info&quot;, //use ::withpath for autolink specific path exampe.com/download/::withpath
+path : &quot;#?o=&quot;
+}; 
 
 // Fix Link Result
 function extractDomain(url) {
