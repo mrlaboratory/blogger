c2689c40241d0b3a984499627df43595921c4c9b
mrlaboratory <mrlaboratory23@gmail.com>
2021-05-30 21:34:57 +0600

    added download page for template site

---
+++ js/downpagefortemplate.js
@@ -0,0 +1,78 @@
+function getQueryVariable(variable)
+{
+
+       var query = window.location.search.substring(1);
+
+       var vars = query.split("&");
+
+       for (var i=0;i<vars.length;i++) {
+
+               var pair = vars[i].split("=");
+
+               if(pair[0] == variable){return pair[1];}
+
+       }
+
+       return(false);
+}
+var time = 40;
+
+var page = getQueryVariable("t");
+function countDown(){
+
+time--;
+
+gett("timecount").innerHTML = time;
+
+if(time == 0){
+    var drivelink = page.split('/');
+    var drivelinkk = drivelink[5];	
+    var finaldrivelinkk = "https://drive.google.com/uc?id="+drivelinkk+"&export=download";
+window.location = finaldrivelinkk;
+}
+
+}
+
+function gett(id){
+
+if(document.getElementById) return document.getElementById(id);
+
+if(document.all) return document.all.id;
+
+if(document.layers) return document.layers.id;
+
+if(window.opera) return window.opera.id;
+
+}
+
+function init(){
+
+if(gett('timecount')){
+
+setInterval(countDown, 1000);
+
+gett("timecount").innerHTML = time;
+
+}
+
+else{
+
+setTimeout(init, 50);
+
+}
+
+}
+
+document.onload = init();
+
+$(document).ready(function(){
+
+ $('.link-out-btn').click(function(){
+
+    window.location = page;
+
+ });
+
+ $('#linkout').text(page);
+
+});
