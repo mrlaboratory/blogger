6f9fa76bed1ba524abc0f1a38ef23f80655f56d7
mrlaboratory <mrlaboratory23@gmail.com>
2021-05-31 17:23:30 +0600

    added fb and wa js

---
+++ js/fbpagewa.js
@@ -0,0 +1,14 @@
+(function () {
+        var options = {
+            facebook: "104806817841044", // Facebook page ID
+            whatsapp: "+8801854650673", // WhatsApp number
+            call_to_action: "Contact Me", // Call to action
+            button_color: "#00608e", // Color of button
+            position: "left", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
