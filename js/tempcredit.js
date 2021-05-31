40e34d2227ba300aba16ccd89744fc7057a86964
mrlaboratory <mrlaboratory23@gmail.com>
2021-05-31 20:42:18 +0600

    addded my temp credit

---
+++ js/tempcredit.js
@@ -0,0 +1,15 @@
+// don't Remove my credit 
+/* This template redesign redevelopment by me 
+Name : Md Mijanur Rahaman
+Facebook: fb.com/mdmijanurrahaman.mr
+Number : +8801854650673
+Website : mrlaboratory.com
+*/
+window.onload = function(){
+    if(document.getElementById("footerc") == null){
+ window.location.href = "https://www.mrlaboratory.com";
+ }else{
+var cr = document.querySelector('#footerc');
+cr.innerHTML="<a href='https://www.mrlaboratory.com'>MR Laboratory</a>";
+    }
+};
