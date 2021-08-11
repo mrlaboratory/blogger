4e6b4640243e80e7b525b31e2e8a47070ca4afe5
MR Laboratory <58909573+mrlaboratory@users.noreply.github.com>
2021-08-11 13:41:28 +0600

    Update tempweb.js

--- js/tempweb.js
+++ js/tempweb.js
@@ -6,6 +6,7 @@ var tempcheck3MR = document.getElementById("tempcheckLink3");
 var tempcheck4MR = document.getElementById("tempcheckLink4");
 var tempcheck5MR = document.getElementById("tempcheckLink5");
 var tempcheck6MR = document.getElementById("tempcheckLink6");
+var tempcheck7MR = document.getElementById("tempcheckLink7");
 var altersafePreview = document.querySelector(".safePreview");
 var altersafeDownload = document.querySelector(".safeDownload");
 var modifytempDemoLink ="https://template.mrlaboratory.info/p/template.html?u="+tempDemoLink;
@@ -18,11 +19,15 @@ var tempMResponsive ="https://www.responsinator.com/?url="+tempDemoLink;
 var tempGTest ="https://search.google.com/test/rich-results?url="+tempDemoLink;
 var tempAllResponsive ="https://responsivedesignchecker.com/checker.php?url="+tempDemoLink;
 var tempmockup6 ="https://mockup.mrlaboratory.info/?url="+tempDemoLink;
+var tempschema7 ="https://validator.schema.org/#url="+tempDemoLink;
 tempcheck1MR.href=tempMobileF;
 tempcheck2MR.href=tempspeed;
 tempcheck3MR.href=tempMResponsive;
 tempcheck4MR.href=tempGTest;
 tempcheck5MR.href=tempAllResponsive;
 tempcheck6MR.href=tempmockup6;
+tempcheck7MR.href=tempschema7;
 altersafePreview.href=tempmockup6;
 altersafeDownload.href=temDownLink;
+
+
