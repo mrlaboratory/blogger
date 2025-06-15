b66abd15b2ebd1b1ce9ca6cef642249482add523
Md Mijanur Rahaman <58909573+mrlaboratory@users.noreply.github.com>
2025-06-15 23:05:29 +0600

    Update dtemplate.js

--- js/dtemplate.js
+++ js/dtemplate.js
@@ -28,3 +28,32 @@ downloadmrtemp.style.cssText='opacity: 100%; width: 250px; height: 100px; positi
 document.styleSheets[0].insertRule('\ @keyframes tempanimamr {\ 0%{bottom: 20px;}\ 20%{bottom: 70px}\ 50%{bottom: 50px}\ 100%{bottom: 50px}\ }'
 );
 }
+
+/**
+ * Developer Credit Script
+ * 
+ * Developed by: Md Mijanur Rahaman
+ * LinkedIn: https://www.linkedin.com/in/mijanurrahaman
+ * GitHub: https://github.com/mrlaboratory
+ * Website: https://mrlaboratory.github.io
+ * Business: Bismillah IT House, Shantirhat, Patiya, Chittagong
+ * Business Website: https://bismillahithouse.com/
+ * WhatsApp: https://wa.me/8801854650673
+ * Contact: mrlaboratory23@gmail.com | +8801854650673
+ * 
+ * Description: This script displays a colorful developer credit in the console.
+ */
+
+(function() {
+    const creditMessage = `\n%c🚀 Developed by: %cMd Mijanur Rahaman\n%c🔗 LinkedIn: %chttps://www.linkedin.com/in/mijanurrahaman\n%c💻 GitHub: %chttps://github.com/mrlaboratory\n%c🌐 Website: %chttps://mrlaboratory.github.io\n%c🏢 Business: %cBismillah IT House, Shantirhat, Patiya, Chittagong\n%c🛍 Business Website: %chttps://bismillahithouse.com/\n%c📞 WhatsApp: %chttps://wa.me/8801854650673\n%c✉️ Contact: %cmrlaboratory23@gmail.com | +8801854650673\n`;
+    
+    console.log(creditMessage, 
+        'color: yellow; font-size: 22px; font-weight: bold;', 'color: cyan; font-size: 22px; font-weight: bold;', 
+        'color: orange; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;',
+        'color: green; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;',
+        'color: blue; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;',
+        'color: purple; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;',
+        'color: red; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;',
+        'color: brown; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;',
+        'color: pink; font-size: 18px; font-weight: bold;', 'color: white; font-size: 18px;');
+})();
